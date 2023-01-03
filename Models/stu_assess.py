4757b4edf2bf2a86082cc7fd9d882d457ebfd39b
ipcampbell12 <ipcampbell12@gmail.com>
2023-01-03 10:20:07 -0800

    fixed models

--- Models/stu_assess.py
+++ Models/stu_assess.py
@@ -4,6 +4,6 @@ class StudentsAssessments(db.Model):
     __tablename__ = "students_assessments"
 
     id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Columnt(db.Integer,db.ForeignKey("students.id"))
+    student_id = db.Column(db.Integer,db.ForeignKey("students.id"))
     assessment_id = db.Column(db.Integer,db.ForeignKey("assessments.id"))
     score = db.Column(db.Integer, nullable=False)
