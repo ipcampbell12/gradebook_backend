0ea69482cf0b8d756a1707e5d290603628d245f1
ipcampbell12 <ipcampbell12@gmail.com>
2023-02-08 13:53:51 -0800

    changed student to students

--- Models/student.py
+++ Models/student.py
@@ -11,7 +11,7 @@ class StudentModel(db.Model):
     #one to many relationship with teachers (child)
     teacher = db.relationship("TeacherModel",back_populates="students")
     
-    assessments = db.relationship("StudentsAssessments",back_populates="student")
+    assessments = db.relationship("StudentsAssessments",back_populates="students")
     #Should that be "students" instead of "student"?
 
     # # one ot many relationship with grades (parent)
--- start_dev.py
+++ start_dev.py
@@ -1,4 +1,4 @@
-from project import create_app
+from app import create_app
 from dotenv import load_dotenv
 from instance.config import Config
 
