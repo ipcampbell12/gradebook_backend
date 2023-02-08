0077d1a767f116a2dfef898f9036a8932d6cb3aa
ipcampbell12 <ipcampbell12@gmail.com>
2023-02-07 16:31:30 -0800

    no more debug mode

--- start_dev.py
+++ start_dev.py
@@ -2,7 +2,7 @@ from app import create_app
 from dotenv import load_dotenv
 from instance.config import Config
 
-app = create_app(Config)
+app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5001)
